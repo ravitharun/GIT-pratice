facb5828af25b628fa3230ef908ff19b06475ffb
Ravi Tharun <tr565003@gmail.com>
2025-11-11 19:48:08 +0530

    updated the feature erro/msg

--- Two.js
+++ Two.js
@@ -1,6 +1,9 @@
 import userData from './One.js'
-if(!userData.Name){
-    alert("NAme is requird")
+if (!userData.Name) {
+    alert("Name is requird")
 }
+else {
 
-console.log(`My Name is ${userData.Name} and my age is ${userData.Age} and email is ${userData.email}`)
+
+    console.log(`My Name is ${userData.Name} and my age is ${userData.Age} and email is ${userData.email}`)
+}
